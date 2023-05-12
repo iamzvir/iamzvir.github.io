f00baf2149dadc06a6d8affe3e6719709c6746d7
iamzvir <78554006+iamzvir@users.noreply.github.com>
2023-05-13 02:16:52 +0300

    Add files via upload

---
+++ 08_05_23_Cappadocia/js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+  
+  $('.fairy-slider, .our-slider').slick({
+    prevArrow: '<button type="button" class="slick-prev slick-btn slick-arrow-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn slick-arrow-next"><img src="images/arrow-next.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          arrows: false,
+          dots: true,
+        }
+      },
+    ]
+  });
+
+})
+
+  
