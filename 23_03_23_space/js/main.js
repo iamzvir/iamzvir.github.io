75c611ec567ffe8239f4b667ee323ad525a0beab
iamzvir <78554006+iamzvir@users.noreply.github.com>
2023-05-17 22:31:30 +0300

    Add files via upload

---
+++ 23_03_23_space/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+
+  $('.slider__box').slick({
+    // здесь мы можем заменить всю строку <button type="button" class="slick-prev">Previous</button> на свою:
+
+    prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow-left">',
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow-right">'
+
+  });
+
+  $('.burger-menu').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--open');
+  })
+
+});
