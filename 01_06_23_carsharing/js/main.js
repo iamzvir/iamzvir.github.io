316bab1703897414d56a5e716f8a04d4de98378a
iamzvir <78554006+iamzvir@users.noreply.github.com>
2023-06-02 17:26:58 +0300

    Add files via upload

---
+++ 01_06_23_carsharing/js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+
+
+  $('.offer-top__slider').slick({
+    arrows: false,
+    dots: true,
+    dotsClass: 'offer-top__dots',
+    fade: true,
+  });
+
+  $('.reviews__slider').slick({
+    arrows: false,
+    dots: true,
+    dotsClass: 'reviews__dots',
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    
+  });
+
+
+})
